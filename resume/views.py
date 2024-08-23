32074d06cc04a2c3419b9febd6ffe5b0b8339fd5
Carlos J Ibanez <77297452+carlosj1999@users.noreply.github.com>
2024-08-22 20:18:53 -0400

    Add files via upload

---
+++ resume/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+#class HomeView(TemplateView):
+#    template_name = 'jobs/home.html'
+
+def home(request):
+    return render(request, 'resume/home.html', {})
+
+def resume(request):
+    return render(request, 'resume/resume.html', {})
