57673c2710c822e60b7f6b56538533f549057299
carlosj1999 <77297452+carlosj1999@users.noreply.github.com>
2024-09-18 14:33:19 -0400

    test 2 to remove extra info in signup

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -6,3 +7,11 @@ class CustomSignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']  # Username and password fields only
+
+    def __init__(self, *args, **kwargs):
+        super(CustomSignupForm, self).__init__(*args, **kwargs)
+        # Ensure no other unwanted fields are added
+        unwanted_fields = ['password_base_authentication', 'password_authentication_enabled']
+        for field in unwanted_fields:
+            if field in self.fields:
+                del self.fields[field]
